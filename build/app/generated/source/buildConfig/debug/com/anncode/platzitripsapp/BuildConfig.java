25ed0ff32d222b5ad5b032d11692860492cb3ad4
Sebastian Chicoma <sebastianchicoma97@gmail.com>
2019-12-29 18:26:25 -0500

    Clase 11, agregando librerías para integrar el proyecto con firebase

--- build/app/generated/source/buildConfig/debug/com/anncode/platzitripsapp/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/anncode/platzitripsapp/BuildConfig.java
@@ -5,7 +5,7 @@ package com.anncode.platzitripsapp;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.anncode.platzitripsapp";
+  public static final String APPLICATION_ID = "com.schicoma.platzitripsapp";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
--- lib/platzi_trips.dart
+++ lib/platzi_trips.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'home_trips.dart';
-import 'search_trips.dart';
-import 'profile_trips.dart';
+import 'package:platzi_trips_app/place/ui/screens/home_trips.dart';
+import 'package:platzi_trips_app/place/ui/screens/search_trips.dart';
+import 'package:platzi_trips_app/user/ui/screens/profile_trips.dart';
 
 class PlatziTrips extends StatefulWidget {
   @override
